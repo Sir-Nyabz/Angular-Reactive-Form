9d3d5fc3834592a9ab5288a48ec7454d2f339c39
Isaac Nyaabila <nyaabilai@gmail.com>
2023-12-13 15:52:10 +0000

    5

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -8,21 +8,52 @@ import { FormControl,FormGroup,ReactiveFormsModule } from '@angular/forms';
 })
 export class SignUpComponent implements OnInit {
 
-  username = new FormControl("")
 
   userForm : FormGroup
+  stateOptions:string[]=['PA','QH','MI']
+
+  userAddressInfo:any={
+    street : 'Nii Ablor Mills Link',
+    city:'My city',
+    state:this.stateOptions[1],
+    zip:'+233'
+}
 
   constructor() { }
 
   ngOnInit() {
     this.userForm=new FormGroup({
+      username : new FormControl(""),
+      password : new FormControl(""),
+      confirmPassword : new FormControl(""),
 
+      address:new FormGroup({
+        street : new FormControl(""),
+      city : new FormControl(""),
+      state : new FormControl(""),
+      zip : new FormControl("")
+      })
+    })
+  }
+
+  autoFillAddress(){
+    this.userForm.patchValue({
+      address:{
+        street: this.userAddressInfo.street,
+        city: this.userAddressInfo.city,
+        state: this.userAddressInfo.state,
+        zip: this.userAddressInfo.zip
+      }
     })
   }
 
   clear(){
-    //this.username.reset
-    this.username.setValue("")
+    this.userForm.reset()
+    //this.username.setValue("")
+  }
+
+  onSubmit(){
+    console.log(this.userForm.value)
   }
 
 }
